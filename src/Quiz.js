8949af24ed5f6e89a5c7fa96c34620ec91b93131
thomasRD08 <44640015+thomasRD08@users.noreply.github.com>
2019-02-04 14:19:53 +0100

    fifth commit

--- src/Quiz.js
+++ src/Quiz.js
@@ -16,8 +16,15 @@ class Quiz extends Component{
   (this.state.quiz_position - 1) === quizData.quiz_questions.length;
     return( <div id="root">
       {isQuizEnd ? <QuizEnd /> :
-      <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>}
+      <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}
+      showNextQuestionHandler={this.showNextQuestion.bind(this)}/>}
     </div>);
+  }
+
+  showNextQuestion(){
+    this.setState((state) => {
+      return {quiz_position: state.quiz_position + 1 }
+    });
   }
 
 };
