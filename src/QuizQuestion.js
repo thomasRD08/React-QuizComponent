e3fdbd0f9faed644343989b04bca99328a0c7bc4
thomasRD08 <44640015+thomasRD08@users.noreply.github.com>
2019-02-04 12:47:24 +0100

    forth commit

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -10,7 +10,6 @@ class QuizQuestion extends Component{
         </section>
         <section className="buttons">
           <ul>
-            //<li>{this.props.quiz_question.answer_options[0]}</li>
             <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
           </ul>
         </section>
